923e29e0429e2624e4aa48fe1e6aebbfc6367576
rodtaira-movecode <rodrigotaira@movecode.com>
2020-03-04 12:27:05 -0300

    Removing files

--- src/utils/ifError.js
+++
@@ -1,8 +0,0 @@
-const {errorHandler} = require('../helpers/dbErrorHandler')
-
-module.exports = function ifError(err, res){
-
-        return res.status(400).json({
-            error: errorHandler(err)
-        })   
-}
